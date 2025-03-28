90b4cb7235ce08b0850c9ce87681586f4d79abf8
tbattur22 <tbattur22@yahoo.com>
2025-03-28 14:55:54 -0500

    Naming routes and using named routes for href links.

--- resources/views/components/nav-menu.blade.php
+++ resources/views/components/nav-menu.blade.php
@@ -2,11 +2,11 @@
 $activePattern = isset($patternObj) ? $patternObj->getName() : null;
 @endphp
 
-<li @class(['border-b border-black-400'=>!$activePattern])><a href="/">Home</a></li>
+<li @class(['border-b border-black-400'=>!$activePattern])><a href="{{ route('home') }}">Home</a></li>
 @isset($patterns)
     @foreach ($patterns as $pattern)
         <li @class(['border-b border-black-400'=>$activePattern === $pattern->getName()])>
-            <a href="/design_pattern/{{ $pattern->getName() }}"
+            <a href="{{ route('designPattern', $pattern->getName()) }}"
                 {{ $pattern->getName() }}>{{ $pattern->getLabel() }}</a>
         </li>
     @endforeach
--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,9 @@
 use App\Http\Controllers\DesignPatternsController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [DesignPatternsController::class, 'index']);
+Route::get('/', [DesignPatternsController::class, 'index'])->name("home");
 
-Route::get('/design_pattern/{pattern}', [DesignPatternsController::class, 'designPattern']);
+Route::get('/design_pattern/{pattern}', [DesignPatternsController::class, 'designPattern'])->name("designPattern");
 
 /**
  * Ajax request handling
